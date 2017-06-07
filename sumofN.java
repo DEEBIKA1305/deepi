10c1a2e12f304a64250152f29ebf9cf7e8f2d95b
DEEBIKA1305 <tdeepika4eee@gmail.com>
2017-06-07 11:14:19 +0530

    Add files via upload

---
+++ sumofN.java
@@ -0,0 +1,15 @@
+package dp;
+import java.util.*;
+public class sumofN {
+	public static void main(String args[]){
+		Scanner s=new Scanner(System.in);
+		int sum=0;
+	    System.out.println("Enter the number of integers to be added:");
+		int num=s.nextInt();
+		for(int i=1;i<=num;i++){
+			 sum=sum+i;
+		}
+		System.out.println("Sumof the natural numbers are:"+sum);
+	}
+
+}
